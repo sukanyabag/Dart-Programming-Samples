2f7f004fe7560d12c8cfa3be30e212a735714bf1
Sukanya Bag <sukannya472@gmail.com>
2021-09-12 09:06:51 +0530

    Create const_var.dart

---
+++ Variables in Dart/const_var.dart
@@ -0,0 +1,11 @@
+// main entry point
+// note - everything in dart is an object
+main(List<String> arguments){
+  
+  String ant = 'Ant';
+  const String wasp = 'Wasp'; //flagging a var as const, can never change the var val
+
+  print(ant + ' man' + ' and' + ' The ' + wasp);
+  
+}
+
