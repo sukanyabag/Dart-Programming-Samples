67d3576ca9bf2d5dd7effba405ffacdf37fb3f94
Sukanya Bag <sukannya472@gmail.com>
2021-09-12 11:28:02 +0530

    Create enumerator.dart

---
+++ Collections in Dart/enumerator.dart
@@ -0,0 +1,8 @@
+enum algos {naivebayes, cnn, vggnet, resnet, gpt3} //enums should be declared outside main fnc
+// main entry point
+// note - everything in dart is an object
+main(List<String> arguments){
+   
+   print(algos.values);
+
+}
