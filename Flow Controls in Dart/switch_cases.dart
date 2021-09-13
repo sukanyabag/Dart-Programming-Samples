217ca7cb4f2597c22693c1bde26c8919516e7708
Sukanya Bag <sukannya472@gmail.com>
2021-09-13 10:04:37 +0530

    Create switch_cases.dart

---
+++ Flow Controls in Dart/switch_cases.dart
@@ -0,0 +1,25 @@
+main(List<String> arguments){
+  
+  int age = 18;
+
+  switch(age){
+    case 18:
+      print('you are 18, you can vote.');
+      break;
+    
+    case 21:
+      print('you are 21, you are an adult.');
+      break;
+
+
+    case 65:
+      print('you are 65, you can retire and chill.');
+      break;
+
+    default:
+      print('Age is just a number, enjoy as long as you are alive');
+      break;   
+
+  }
+ 
+}
