430da9b5b07c0e1895afdf6c627f7e99fe276ccd
Sukanya Bag <sukannya472@gmail.com>
2021-09-13 09:18:08 +0530

    Create assert.dart

---
+++ Flow Controls in Dart/assert.dart
@@ -0,0 +1,11 @@
+// main entry point
+// note - everything in dart is an object
+
+main(List<String> arguments){
+  
+  print('starting');
+  int age = 41;
+  assert(age == 41);
+  print('finished!');
+
+}
